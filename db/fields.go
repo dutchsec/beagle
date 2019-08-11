1aaae2cad832fbc40f543b424291c39e667ddbf2
Remco Verhoef <remco@dutchcoders.io>
2019-08-11 22:11:45 +0200

    use type Field instaed of string for fields

--- db/fields.go
+++ db/fields.go
@@ -13,32 +13,8 @@
 // limitations under the License.
 package db
 
-// Where TODO: NEEDS COMMENT INFO
-/*
-func (qx Queryx) Fields(fields ...string) Queryx {
-	q := string(qx.Query)
-	params := qx.Params
-
-	q, params = Fields(fields).Wrap(q, params)
-
-	return Queryx{
-		Query(q),
-		params,
-	}
+// Fields NEEDS COMMENT INFO
+func (tq Queryx) Fields(fields ...Field) Queryx {
+	tq.fields = fields
+	return tq
 }
-
-func Fields(fields []string) selectOption {
-	return &fieldsOption{fields}
-}
-
-type fieldsOption struct {
-	fields []string
-}
-
-// Wrap TODO: NEEDS COMMENT INFO
-func (o *fieldsOption) Wrap(query string, params []interface{}) (string, []interface{}) {
-	query = fmt.Sprintf("SELECT %s FROM (%s) a", strings.Join(o.fields, ","), query)
-	return query, params
-}
-
-*/
